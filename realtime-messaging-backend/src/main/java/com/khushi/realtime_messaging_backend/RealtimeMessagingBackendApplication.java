8610a1651537c230a11b28e557921f9839e8ccb1
Khushi Sawalkar <khushi27sawalkar@gmail.com>
2026-03-11 01:19:38 +0530

    Initialize Spring Boot project

---
+++ realtime-messaging-backend/src/main/java/com/khushi/realtime_messaging_backend/RealtimeMessagingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khushi.realtime_messaging_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeMessagingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeMessagingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khushi/realtime_messaging_backend/RealtimeMessagingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khushi.realtime_messaging_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeMessagingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
